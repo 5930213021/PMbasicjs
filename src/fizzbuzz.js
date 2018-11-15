942fabb1812667e025bcb291a7682ce288630288
nisarat <nisaratnsaelim@gmail.com>
2018-11-15 20:21:40 +0700

    add

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,17 @@
 module.exports.fizzbuzz = numebr  => {
-    if(numebr == 1) return 1;
-    if(numebr == 2) return 2;
-    if(numebr == 3) return "Fizz";
-    if(numebr == 4) return 4;
-    if(numebr == 5) return "Buzz";
-    if(numebr == 6) return "Fizz";
-    
+    if(number %3===0&&numebr%5!=0) return "Buzz";
+    else if(number %5===0&&number%3!==0) return "Fizz";
+    else if(number %5===0&&number%3===0) return "FizzBuzz";
+    else return number;
+
+    // if(numebr == 1) return 1;
+    // if(numebr == 2) return 2;
+    // if(numebr == 3) return "Fizz";
+    // if(numebr == 4) return 4;
+    // if(numebr == 5) return "Buzz";
+    // if(numebr == 6) return "Fizz";
+    // if(numebr == 7) return 7;
+    // if(numebr == 8) return 8;
+    // if(numebr == 9) return "Fizz";
 
 }
--- tests/unit/fizzbuzz.test.js
+++ tests/unit/fizzbuzz.test.js
@@ -30,4 +30,74 @@ describe('fizzbuzz',()=>{
         const result = libs.fizzbuzz(number);
         expect(result).toBe("Fizz"); 
     })
+    it('should return 7 if number is 7',() =>{
+        const number = 7;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe(7); 
+    })
+    it('should return 8 if number is 8',() =>{
+        const number = 8;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe(8); 
+    })
+    it('should return Fizz if number is 9',() =>{
+        const number = 9;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe("Fizz"); 
+    })
+    it('should return Buzz if number is 10',() =>{
+        const number = 10;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe("Buzz"); 
+    })
+    it('should return 11 if number is 11',() =>{
+        const number = 11;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe(11); 
+    })
+    it('should return Fizz if number is 12',() =>{
+        const number = 12;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe("Fizz"); 
+    })
+    it('should return 13 if number is 13',() =>{
+        const number = 13;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe(13); 
+    })
+    it('should return 14 if number is 14',() =>{
+        const number = 14;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe(14); 
+    })
+    it('should return FizzBuzz if number is 15',() =>{
+        const number = 15;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe("FizzBuzz"); 
+    })
+    it('should return FizzBuzz if number is 30',() =>{
+        const number = 30;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe("FizzBuzz"); 
+    })
+    it('should return Buzz if number is 40',() =>{
+        const number = 40;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe("Buzz"); 
+    })
+    it('should return Fizz if number is 66',() =>{
+        const number = 66;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe("Fizz"); 
+    })
+    it('should return Fizz if number is 87',() =>{
+        const number = 87;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe("Fizz"); 
+    })
+    it('should return Buzz if number is 100',() =>{
+        const number = 100;
+        const result = libs.fizzbuzz(number);
+        expect(result).toBe("Buzz"); 
+    })
 })
