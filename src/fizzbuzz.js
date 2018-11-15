e41774c65642a705b8cc7000539b55956b38a456
nisarat <nisaratnsaelim@gmail.com>
2018-11-15 20:02:41 +0700

    add

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,3 +1,10 @@
 module.exports.fizzbuzz = numebr  => {
-    return 0;
+    if(numebr == 1) return 1;
+    if(numebr == 2) return 2;
+    if(numebr == 3) return "Fizz";
+    if(numebr == 4) return 4;
+    if(numebr == 5) return "Buzz";
+    if(numebr == 6) return "Fizz";
+    
+
 }
